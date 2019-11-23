b9342ba91eb18cd70deba72116cfb25dfac8f5e7
CodeJS <jonstrouse@gmail.com>
2019-11-23 10:51:48 -0500

    Latest Updates
    Styled bell notification marker
    Trouble incorporating chart.js

--- app.js
+++ app.js
@@ -15,118 +15,112 @@ alert.addEventListener('click', (event) => {
 });
 
 
-let trafficCanvas = document.getElementById('traffic-chart');
+const trafficCanvas = document.getElementById('traffic-chart');
 
-​let​ trafficData = {
-  labels: [​"16-22"​, ​"23-29"​, ​"30-5"​, ​"6-12"​, ​"13-19"​, ​"20-26"​, ​"27-3"​,
-"4-10"​, ​"11-17"​, ​"18-24"​, ​"25-31"​],
+let trafficdata = {
+  labels: ["16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3", "4-10", "11-17", "18-24", "25-31"],
   datasets: [{
-    data: [​750​, ​1250​, ​1000​, ​2000​, ​1500​, ​1750​, ​1250​, ​1850​, ​2250​, ​1500​, 2500​],
-    backgroundColor: ​'rgba(116, 119, 191, .3)'​,
-    borderWidth: ​1​,
+    data: [750, 1250, 1000, 2000, 1500, 1750, 1250, 1850, 2250, 1500, 2500],
+    backgroundColor: 'rgba(116, 119, 191, .3)',
+    borderWidth: 1,
   }]
 };
 
-​let​ trafficOptions = {
-  aspectRatio: ​2.5​,
+let trafficOptions = {
+  aspectRatio: 2.5,
   animation: {
-    duration: ​0
+    duration: 0
   },
-  scales: {
-    yAxes: [{
-      ticks: {
-        beginAtZero:​true
-      }
-    }]
-  },
-  legend : {
-    display: ​false
-  }
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: true
+        }
+      }]
+    },
+    legend: {
+      display: false
+    }
 };
 
-​let​ trafficChart = ​new​ Chart(trafficCanvas, {
-  type: ​'line'​,
+let trafficChart = new Chart(trafficCanvas, {
+  type: 'line',
   data: trafficData,
   options: trafficOptions
 });
 
-​const​ dailyCanvas = ​document​.getElementById(​"daily-chart"​);
+const dailyCanvas = document.getElementById("daily-chart");
 
-​// data for daily traffic bar chart
-​const​ dailyData = {
-  labels: [​"S"​, ​"M"​, ​"T"​, ​"W"​, ​"T"​, ​"F"​, ​"S"​],
-  datasets: [{
-    label: ​'# of Hits'​,
-    data: [​75​, ​115​, ​175​, ​125​, ​225​, ​200​, ​100​],
-    backgroundColor: ​'#7477BF'​,
-    borderWidth: ​1
+const dailyData = {
+  labels: ["S", "M", "T", "W", "T", "F", "S"], datasets: [{
+    label: '# of Hits',
+    data: [75, 115, 175, 125, 225, 200, 100], backgroundColor: '#7477BF',
+    borderWidth: 1
   }]
 };
 
-​const​ dailyOptions = {
+const dailyOptions = {
   scales: {
     yAxes: [{
       ticks: {
-        beginAtZero:​true
+        beginAtZero: true
       }
     }]
   },
-legend : {
-  display: ​false
+  legend: { display: false
   }
 }
 
-let​ dailyChart = ​new​ Chart(dailyCanvas, {
-  type: ​'bar'​,
+let dailyChart = new Chart(dailyCanvas, {
+  type: 'bar',
   data: dailyData,
   options: dailyOptions
 });
 
-const​ mobileCanvas = ​document​.getElementById(​"mobile-chart"​);
+const mobileCanvas = document.getElementById("mobile-chart");
 
-const​ mobileData = {
-labels: [​"Desktop"​, ​"Tablet"​, ​"Phones"​],
-datasets: [{
-  label: ​'# of Users'​,
-  data: [​2000​, ​550​, ​500​],
-  borderWidth: ​0​,
-  backgroundColor: [
-​    '#7477BF'​, ​
-    '#78CF82'​, ​
-    '#51B6C8'
+const mobileData = {
+  labels: ["Desktop", "Tablet", "Phones"],
+  datasets: [{
+    label: '# of users',
+    data: [2000, 550, 500],
+    borderWidth: 0,
+    backgroundColor: [
+      '##7477BF',
+      '#78CF82',
+      '#51B6C8'
     ]
   }]
 };
 
-​const​ mobileOptions = {
+const mobileOptions = {
   legend: {
-    position: ​'right'​,
+    position: 'right',
     labels: {
-      boxWidth: ​20​,
-      fontStyle: ​'bold'
+      boxWidth: 20,
+      fontStyle: 'bold'
     }
   }
 }
 
-​let​ mobileChart = ​new​ Chart(mobileCanvas, {
-  type: ​'doughnut'​,
+let mobileChart = new Chart(mobileCanvas, {
+  type: 'doughnut',
   data: mobileData,
   options: mobileOptions
 });
 
-const​ user = ​document​.getElementById(​"userField"​);
-const​ message = ​document​.getElementById(​"messageField"​);
-const​ send = ​document​.getElementById(​"send"​);
+const user = document.getElementById("userField");
+const message = document.getElementById("messageField");
+const send = document.getElementById("send");
 
-send.addEventListener(​'click'​, () => {
-​// ensure user and message fields are filled out ​
-  if​ (user.value === ​""​ && message.value === ​""​) {
-    alert(​"Please fill out user and message fields before sending"​);
-  } ​else​ ​if​ (user.value === ​""​ ) {
-    alert(​"Please fill out user field before sending"​);
-  } ​else​ ​if​ (message.value === ​""​ ) {
-    alert(​"Please fill out message field before sending"​);
-  } ​else​ {
-    alert(​`Message successfully sent to: ${user.value}`​);
+send.addEventListener('click', () => {
+  if (user.value === "" && message.value === "") {
+    alert("Please fill out user and message fields before sending");
+  } else if (user.vaule === "") {
+    alert("Please fill our user field before sending");
+  } else if (message.value === "") {
+    alert("Please fill out message field before sending");
+  } else {
+    alert(`Message successfully sent to: ${user.value}`);
   }
 });
