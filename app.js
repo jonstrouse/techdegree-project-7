a40cf4430d62aeb5a48b47a5fc8cbaaaf36ab4c3
CodeJS <jonstrouse@gmail.com>
2019-12-27 10:12:30 -0500

    Commit Bug Fixes

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ alert.addEventListener('click', (event) => {
 
 const trafficCanvas = document.getElementById('traffic-chart');
 
-let trafficdata = {
+let trafficData = {
   labels: ["16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3", "4-10", "11-17", "18-24", "25-31"],
   datasets: [{
     data: [750, 1250, 1000, 2000, 1500, 1750, 1250, 1850, 2250, 1500, 2500],
